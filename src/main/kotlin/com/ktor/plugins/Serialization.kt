8383a220ae5f724a1014ec23cb34ab1d22090a1a
fenil09 <backupsystem9909@gmail.com>
2024-01-31 03:24:41 -0800

    Initial commit

---
+++ src/main/kotlin/com/ktor/plugins/Serialization.kt
@@ -0,0 +1,11 @@
+package com.ktor.plugins
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+    }
+}
